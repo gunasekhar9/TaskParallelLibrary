3025e373aabb28d7b6a98f269bbf0eaf7b154ea9
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-23 14:53:28 +0530

    [gunasekhar9] add : Task Parallel Library

---
+++ TaskParallelLibrary/TaskParallelLibrary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations op = new Operations();
+            string[] words = op.CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+            #region ParallelTasks
+            Parallel.Invoke(
+                   () =>
+                   {
+                       Console.WriteLine("Begin First Task ...");
+                       op.GetLongestWord(words);
+                   },
+                   () =>
+                   {
+                       Console.WriteLine("Begin Second Task ...");
+                       op.GetMostCommonWords(words);
+                   },
+                   () =>
+                   {
+                       Console.WriteLine("Begin Third Task ...");
+                       op.GetCountForWord(words, "sleep");
+                   }
+                );
+            Console.WriteLine("Returned From Parallel.Invoke");
+            #endregion
+        }
+    }
+}
